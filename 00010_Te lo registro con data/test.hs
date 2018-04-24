5a03013834ebce828e79acc82bc809d041fe4eb7
franco@mumuki.org <franco@mumuki.org>
2018-04-24 15:52:51 -0400

    Mumuki Export on 2018-04-24 15:52:51 -0400

--- 00010_Te lo registro con data/test.hs
+++ 00010_Te lo registro con data/test.hs
@@ -1,3 +1,3 @@
 describe "" $ do
   it "vender deberia agregar una venta a la lista" $ do
-    vender "jean libis" "mataderos" [] `shouldBe` [("jean libis", "mataderos")]
+    vender "jean libis" "mataderos" [] `shouldBe` [(Venta "jean libis"  "mataderos")]
