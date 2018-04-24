25655602278b8031c8f0a4fa00a35697e02da366
franco@mumuki.org <franco@mumuki.org>
2018-04-24 16:43:40 -0400

    Mumuki Export on 2018-04-24 16:43:40 -0400

--- 00011_No determinismo glorioso, parte 2/test.hs
+++ 00011_No determinismo glorioso, parte 2/test.hs
@@ -3,4 +3,4 @@ describe "" $ do
     length parGlorioso `shouldBe` 6
     
   it "parGlorioso incluye (omar, oso)" $ do
-    elem ("omar", "oso") parGlorioso `shouldBe` 6
+     parGlorioso `shouldContain` ("omar", "oso")
