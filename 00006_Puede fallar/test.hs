bfdee3a1f0d9168f8cc53823a2c6bbb7f5ca045e
franco@mumuki.org <franco@mumuki.org>
2018-04-24 16:48:55 -0400

    Mumuki Export on 2018-04-24 16:48:55 -0400

--- 00006_Puede fallar/test.hs
+++ 00006_Puede fallar/test.hs
@@ -1,9 +1,9 @@
 describe "" $ do
   it "raizCuadradaReal 4" $ do
-    (raizCuadradaReal 4) `shouldBe` (Just 2)
+    (raizCuadradaReal 4) `shouldBe` (Just 2.0)
   
   it "raizCuadradaReal 100" $ do
-    (raizCuadradaReal 100) `shouldBe` (Just 10)
+    (raizCuadradaReal 100) `shouldBe` (Just 10.0)
     
   it "raizCuadradaReal (-1)" $ do
     (raizCuadradaReal (-1)) `shouldBe` Nothing
